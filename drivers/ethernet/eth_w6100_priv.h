dcb2891624174a1107938a5cbf3c93752f194627
Archey Barrell <a@archbar.me>
2025-12-27 02:08:45 +0000

    Add init

--- drivers/ethernet/eth_w6100_priv.h
+++ drivers/ethernet/eth_w6100_priv.h
@@ -12,70 +12,34 @@
 #ifndef ETH_W6100_PRIV_H_
 #define ETH_W6100_PRIV_H_
 
-/*
- * W6100 common registers
- */
-#define W6100_COMMON_REGS	0x0000
-#define W6100_MR		0x0000 /* Mode Register */
-#define W6100_GW		0x0001
-#define MR_RST			0x80 /* S/W reset */
-#define MR_PB			0x10 /* Ping block */
-#define MR_AI			0x02 /* Address Auto-Increment */
-#define MR_IND			0x01 /* Indirect mode */
-#define W6100_SHAR		0x0009 /* Source MAC address */
-#define W6100_IR		0x0015 /* Interrupt Register */
-#define W6100_COMMON_REGS_LEN	0x0040
-#define W6100_PHYCFGR		0x002E /* PHY Configuration register */
+// Common Register Locations
+#define W6100_SYSR 0x2000  /* System Configuration Register */
+#define W6100_SYCR0 0x2004 /* System Configuration Register 0 aka Soft Reset */
+#define W6100_SIMR 0x2214  /* Socket Interrupt Mask Register */
+#define W6100_RTR 0x4200   /* Retry Time Register */
+#define W6100_SHAR 0x4120  /* Source MAC Address (len 6) */
 
-#define W6100_Sn_MR		0x0000 /* Sn Mode Register */
-#define W6100_Sn_CR		0x0001 /* Sn Command Register */
-#define W6100_Sn_IR		0x0002 /* Sn Interrupt Register */
-#define W6100_Sn_SR		0x0003 /* Sn Status Register */
-#define W6100_Sn_TX_FSR		0x0020 /* Sn Transmit free memory size */
-#define W6100_Sn_TX_RD		0x0022 /* Sn Transmit memory read pointer */
-#define W6100_Sn_TX_WR		0x0024 /* Sn Transmit memory write pointer */
-#define W6100_Sn_RX_RSR		0x0026 /* Sn Receive free memory size */
-#define W6100_Sn_RX_RD		0x0028 /* Sn Receive memory read pointer */
+// Common Register values
+#define RTR_DEFAULT 2000   /* Expected default RTR value */
+#define SIMR_S0 0x01 /* Socket Interrupt Mask Register for Socket 0 */
 
-#define W6100_S0_REGS		0x10000
+// Socket Register Locations
+#define W6100_Sn(n, reg_offset) \
+		(0x10000 + (n) * 0x40000 + (reg_offset))
 
-#define W6100_S0_MR		(W6100_S0_REGS + W6100_Sn_MR)
-#define S0_MR_MACRAW		0x04 /* MAC RAW mode */
-#define S0_MR_MF		0x40 /* MAC Filter for W6100 */
-#define W6100_S0_CR		(W6100_S0_REGS + W6100_Sn_CR)
-#define S0_CR_OPEN		0x01 /* OPEN command */
-#define S0_CR_CLOSE		0x10 /* CLOSE command */
-#define S0_CR_SEND		0x20 /* SEND command */
-#define S0_CR_RECV		0x40 /* RECV command */
-#define W6100_S0_IR		(W6100_S0_REGS + W6100_Sn_IR)
-#define S0_IR_SENDOK		0x10 /* complete sending */
-#define S0_IR_RECV		0x04 /* receiving data */
-#define W6100_S0_SR		(W6100_S0_REGS + W6100_Sn_SR)
-#define S0_SR_MACRAW		0x42 /* mac raw mode */
-#define W6100_S0_TX_FSR		(W6100_S0_REGS + W6100_Sn_TX_FSR)
-#define W6100_S0_TX_RD		(W6100_S0_REGS + W6100_Sn_TX_RD)
-#define W6100_S0_TX_WR		(W6100_S0_REGS + W6100_Sn_TX_WR)
-#define W6100_S0_RX_RSR		(W6100_S0_REGS + W6100_Sn_RX_RSR)
-#define W6100_S0_RX_RD		(W6100_S0_REGS + W6100_Sn_RX_RD)
-#define W6100_S0_IMR		(W6100_S0_REGS + W6100_Sn_IMR)
+// Use with W6100_Sn_REGISTER(n, <register offset>)
+#define W6100_Sn_MR 0x0000 /* Socket Mode Register */
+#define W6100_Sn_CR 0x0010 /* Socket Control Register */
+#define W6100_Sn_TX_BSR 0x2000 /* Socket n TX Buffer Size Register */
+#define W6100_Sn_RX_BSR 0x2200 /* Socket n RX Buffer Size Register */
 
-#define W6100_S0_MR_MF		7 /* MAC Filter for W6100 */
-#define W6100_Sn_REGS_LEN	0x0040
-#define W6100_SIMR		0x0018 /* Socket Interrupt Mask Register */
-#define IR_S0			0x01
-#define RTR_DEFAULT		2000
-#define W6100_RTR		0x0019 /* Retry Time-value Register */
-
-
-#define W6100_Sn_RXMEM_SIZE(n)	\
-		(0x1001e + (n) * 0x40000) /* Sn RX Memory Size */
-#define W6100_Sn_TXMEM_SIZE(n)	\
-		(0x1001f + (n) * 0x40000) /* Sn TX Memory Size */
-
-#define W6100_Sn_TX_MEM_START	0x20000
-#define W6100_TX_MEM_SIZE	0x04000
-#define W6100_Sn_RX_MEM_START	0x30000
-#define W6100_RX_MEM_SIZE	0x04000
+// Socket Register Values
+#define S_MR_MACRAW 0x04 /* Socket Mode Register for MACRAW mode */
+#define S_MR_BRDB 0x40 /* Block broadcast udp packets */
+#define S_CR_OPEN 0x01 /* Socket Control Register for OPEN command */
+#define S_CR_CLOSE 0x10 /* Socket Control Register for CLOSE command */
+#define S_CR_SEND 0x20 /* Socket Control Register for SEND command */
+#define S_CR_RECV 0x40 /* Socket Control Register for RECV command */
 
 /* Delay for PHY write/read operations (25.6 us) */
 #define W6100_PHY_ACCESS_DELAY		26U
@@ -84,6 +48,7 @@ struct w6100_config {
 	struct gpio_dt_spec interrupt;
 	struct gpio_dt_spec reset;
 	int32_t timeout;
+	struct net_eth_mac_config mac_cfg;
 };
 
 struct w6100_runtime {
@@ -99,5 +64,4 @@ struct w6100_runtime {
 	bool link_up;
 	uint8_t buf[NET_ETH_MAX_FRAME_SIZE];
 };
-
 #endif /*ETH_W6100_PRIV_H_*/
